5b9eb61fcb2364e3ca55e80d5464b4e9911760b4
EDWARD IREDIA <edwardiredia18@aol.com>
2022-08-11 16:57:46 +0100

    add a loop

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 function getName() {
-let name = prompt("what is your name")
+    let name = prompt("what is your name")
 
-let nameMessage
+    let nameMessage
 
-if (name === "tim") {
-    nameMessage = "best Tim";
-} else {
-    nameMessage ="Welcome" + name;
+    if (name === "tim") {
+        nameMessage = "best Tim";
+    } else {
+        nameMessage ="Welcome" + name;
+    }
 
-}
-
-return document.write(nameMessage + ", ");
-
-
+    return document.write(nameMessage);
 }
 
 
+function getRating() {
+    let output = "";
+    let rating = prompt ("what is my rating")
+
+    for (let i=0; i < rating; i++){
+        output = output + "<img src='duck.png' alt='' />"
+    }
+    
+
+    return document.write(output) 
+}
